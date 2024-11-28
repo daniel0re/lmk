f49d6e34a0ea516a1006ad7c82d79776393a3385
Daniel Leonardo Ore Campos <daniel0rec98@gmail.com>
2024-11-28 00:36:45 -0500

    Responsivo - Ocultando campos

--- script.js
+++ script.js
@@ -31,12 +31,20 @@ function searchPlayers(data) {
   // Limpiar la tabla antes de mostrar los nuevos resultados
   tableBody.innerHTML = '';
   
+  // Verificar si la pantalla es pequeña (menos de 768px de ancho)
+  const isSmallScreen = window.innerWidth < 768;
+  
   // Iterar sobre los jugadores filtrados y añadirlos a la tabla
   filteredData.forEach(player => {
       const row = document.createElement('tr'); // Crear una nueva fila de tabla
       
       // Iterar sobre los datos del jugador y añadir cada celda
-      player.slice(1, 12).forEach(cell => {
+      player.slice(1, 12).forEach((cell, index) => {
+          // Omitir las columnas "Entrenamientos", "Club", y "Préstamo" si la pantalla es pequeña
+          if (isSmallScreen && (index === 0 || index === 5 || index === 6 || index === 7 || index === 8 || index === 9)) {
+              return; // No añadir estas celdas a la fila
+          }
+          
           const td = document.createElement('td'); // Crear una nueva celda de tabla
           td.innerHTML = highlightMatch(cell, searchTerm); // Resaltar las coincidencias en cada celda
           row.appendChild(td); // Añadir la celda a la fila
